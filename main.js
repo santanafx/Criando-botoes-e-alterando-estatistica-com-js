625a8efe48089eba67ad59baed772b3359a90c48
Lucas Santana Figueiredo <santanafx@hotmail.com>
2022-12-31 13:49:49 -0300

    Adicionado addEventListener click

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 var teste = document.querySelector(".texto");
 teste.textContent = "alterando o texto";
 // dependendo do elemento deve ser usado teste.value = "alterando o texto"; para alterar o texto
+var caixa = document.querySelector(".caixa");
+
+let dizOi = () => {
+  alert("OI");
+};
+
+// function dizOi() {
+//   console.log("OI");
+// }
+
+caixa.addEventListener("click", dizOi);
