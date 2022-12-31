80080fb1455634c02b86205975a6fa36b8f01e82
Lucas Santana Figueiredo <santanafx@hotmail.com>
2022-12-31 14:39:44 -0300

    Adicionado botãos de somar e subtrair em JS

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ teste.textContent = "alterando o texto";
 // dependendo do elemento deve ser usado teste.value = "alterando o texto"; para alterar o texto
 var caixa = document.querySelector(".caixa");
 var caixa2 = document.querySelector(".caixa2");
+
+var menos = document.querySelector(".botao__menos");
+var container = document.querySelector(".container__valor");
+var mais = document.querySelector(".botao__mais");
+
 let dizOi = () => {
   alert("OI");
 };
@@ -17,3 +22,11 @@ caixa2.addEventListener("click", () => {
   console.log("OI");
 });
 // Mesma coisa do exemplo anterior, porém, utilizando função anônima
+
+mais.addEventListener("click", () => {
+  container.value = parseInt(container.value) + 1;
+});
+
+menos.addEventListener("click", () => {
+  container.value = parseInt(container.value) - 1;
+});
