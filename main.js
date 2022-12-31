ba61bc95b4508a0bd691fff4c4cd5b9d0a9d0e28
Lucas Santana Figueiredo <santanafx@hotmail.com>
2022-12-31 14:19:26 -0300

    Criando e utilizando função anônima arrow function

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var teste = document.querySelector(".texto");
 teste.textContent = "alterando o texto";
 // dependendo do elemento deve ser usado teste.value = "alterando o texto"; para alterar o texto
 var caixa = document.querySelector(".caixa");
-
+var caixa2 = document.querySelector(".caixa2");
 let dizOi = () => {
   alert("OI");
 };
@@ -12,3 +12,8 @@ let dizOi = () => {
 // }
 
 caixa.addEventListener("click", dizOi);
+
+caixa2.addEventListener("click", () => {
+  console.log("OI");
+});
+// Mesma coisa do exemplo anterior, porém, utilizando função anônima
