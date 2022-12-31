d145c09a25e4065e5a28d62dafe2de9fd5f10643
Lucas Santana Figueiredo <santanafx@hotmail.com>
2022-12-31 15:52:11 -0300

    Código para manipular todos botões dinâmicamente

--- main.js
+++ main.js
@@ -8,10 +8,15 @@ var menos = document.querySelector(".botao__menos");
 var container = document.querySelector(".container__valor");
 var mais = document.querySelector(".botao__mais");
 
+var controle = document.querySelectorAll(".controle__ajuste");
+
+//LÓGICA PARA CLICAR NA CAIXA E DIZER OI-----------------------------------------
+//função normal
 let dizOi = () => {
   alert("OI");
 };
 
+//arrow function
 // function dizOi() {
 //   console.log("OI");
 // }
@@ -21,6 +26,8 @@ caixa.addEventListener("click", dizOi);
 caixa2.addEventListener("click", () => {
   console.log("OI");
 });
+
+//LÓGICA PARA CLICAR NA CAIXA E DIZER OI--------------------------------------------
 // Mesma coisa do exemplo anterior, porém, utilizando função anônima
 
 // mais.addEventListener("click", () => {
@@ -31,18 +38,38 @@ caixa2.addEventListener("click", () => {
 //   container.value = parseInt(container.value) - 1;
 // });
 
-mais.addEventListener("click", () => {
-  manipulaDados("somar");
+//LÓGICA PARA FUNCIONAR SOMENTE O PRIMEIRO BOTÃO-------------------------------------------------------
+// mais.addEventListener("click", () => {
+//   manipulaDados("somar");
+// });
+
+// menos.addEventListener("click", () => {
+//   manipulaDados("subtrair");
+// });
+
+// var manipulaDados = (operacao) => {
+//   if (operacao === "subtrair") {
+//     container.value = parseInt(container.value) - 1;
+//   } else {
+//     container.value = parseInt(container.value) + 1;
+//   }
+// };
+
+//LÓGICA PARA FUNCIONAR TODOS OS BOTÕES DINÂMICAMENTE---------------------------------------------------
+controle.forEach((elemento) => {
+  //elemento é uma variável qualquer que irá receber o valor do array de acordo com a posicao que ele se encontra
+  elemento.addEventListener("click", (evento) => {
+    //evento faz a mesma coisa que o elemento mas no caso ele é uma variável de uma variável
+    manipulaDados(evento.target.textContent, evento.target.parentNode);
+  });
 });
 
-menos.addEventListener("click", () => {
-  manipulaDados("subtrair");
-});
+var manipulaDados = (operacao, container) => {
+  var contador = container.querySelector(".container__valor");
 
-var manipulaDados = (operacao) => {
-  if (operacao === "subtrair") {
-    container.value = parseInt(container.value) - 1;
+  if (operacao === "-") {
+    contador.value = parseInt(contador.value) - 1;
   } else {
-    container.value = parseInt(container.value) + 1;
+    contador.value = parseInt(contador.value) + 1;
   }
 };
