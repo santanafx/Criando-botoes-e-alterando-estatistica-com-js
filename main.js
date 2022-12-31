2240b71891990f58e10ba61fa4adc7595c163342
Lucas Santana Figueiredo <santanafx@hotmail.com>
2022-12-31 13:27:49 -0300

    Adicionado botões com pseudo, alterando texto JS

---
+++ main.js
@@ -0,0 +1,3 @@
+var teste = document.querySelector(".texto");
+teste.textContent = "alterando o texto";
+// dependendo do elemento deve ser usado teste.value = "alterando o texto"; para alterar o texto
